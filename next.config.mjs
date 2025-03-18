daf77b66c58f02d02d8231797cef4f48b003204c
gvisan_ramp <47226425+oilycheddar@users.noreply.github.com>
2025-03-17 18:41:41 -0600

    Performance
    png →  webp

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,11 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    formats: ['image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
   webpack: (config, { dev, isServer }) => {
     // Ensure development mode is properly set
     if (dev && !isServer) {
