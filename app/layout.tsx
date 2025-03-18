88e322cd063dde5c6c144f09891fbd233f034043
gvisan_ramp <47226425+oilycheddar@users.noreply.github.com>
2025-03-18 15:05:34 -0600

    image pre-loading
    prod was NOT slow before this

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import { Metadata } from 'next';
 import { ThemeColorManager } from '../components/ThemeColorManager';
 
-export const metadata: Metadata = {
-  viewport: {
-    width: 'device-width',
-    initialScale: 1,
-    viewportFit: 'cover',
-  },
-  themeColor: '#1A1A1A',
+export const metadata: Metadata = {};
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  viewportFit: 'cover',
 };
+
+export const themeColor = '#1A1A1A';
 
 export default function RootLayout({
   children,
@@ -17,6 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preload" href="/images/optimized/bg-noise-dune.webp" as="image" />
+        <link rel="preload" href="/images/optimized/page-noise-dune.webp" as="image" />
+        <link rel="preload" href="/images/optimized/dune_logo_noise.webp" as="image" />
+      </head>
       <body>
         <ThemeColorManager />
         {children}
